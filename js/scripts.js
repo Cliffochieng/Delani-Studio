42d83e3752bb5c3265743292636a382b872f7220
Cliffochieng <cliffordochieng844@gmail.com>
2020-08-29 15:24:28 +0300

    add header

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(function(){
+	$("div#design").click(function() {
+		$("#design-image").toggle();
+		$("#design-text").toggle();
+	});
+	$("div#dev").click(function(){
+		$("#dev-image").toggle();
+		$("#dev-text").toggle();
+	});
+	$("div#product").click(function(){
+		$("#product-image").toggle();
+		$("#product-text").toggle();
+	});
+	$("div#jim").hover(function() {
+		$("div#jim-text").toggle();
+	})
+	$("div#bailey").hover(function() {
+		$("div#bailey-text").toggle();
+	});
+	$("div#burned").hover(function(){
+		$("div#burned-text").toggle();
+	});
+	$("div#age").hover(function(){
+		$("div#age-text").toggle();
+	});
+	$("div#elexa").hover(function(){
+		$("div#elexa-text").toggle();
+	});
+	$("div#crystals").hover(function(){
+		$("div#crystals-text").toggle();
+	});
+	$("div#ontario").hover(function(){
+		$("div#ontario-text").toggle();
+	});
+	$("div#dark").hover(function(){
+		$("div#dark-text").toggle();
+	});
+	$("form#contactForm").submit(function(event){
+		event.preventDefault();
+		let name = document.getElementById("name").value;
+		console.log(name);
+		alert(name + " we have received your message. Thank you for reaching out to us.");
+	});
+});
