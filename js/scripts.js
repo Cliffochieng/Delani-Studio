f9bdf1f417b7f0503ad1ad04f6652b791c143a30
Cliffochieng <cliffordochieng844@gmail.com>
2020-08-30 14:48:11 +0300

    add README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,100 @@
-$(function(){
-	$("div#design").click(function() {
-		$("#design-image").toggle();
-		$("#design-text").toggle();
+$(document).ready(function(){
+	$("#development-image").click(function(){
+	  $("#development-image").slideDown('1500').hide('1000');
+	  $("#development").show('1500');
 	});
-	$("div#dev").click(function(){
-		$("#dev-image").toggle();
-		$("#dev-text").toggle();
+	$("#development").click(function(){
+	  $("#development").slideUp('1500');
+	  $("#development-image").slideDown('1500');
 	});
-	$("div#product").click(function(){
-		$("#product-image").toggle();
-		$("#product-text").toggle();
+  });
+  
+  $(document).ready(function(){
+	$("#design-image").click(function(){
+	  $("#design-image").slideDown('1500').hide('1000');
+	  $("#design").show('1500');
 	});
-	$("div#jim").hover(function() {
-		$("div#jim-text").toggle();
-	})
-	$("div#bailey").hover(function() {
-		$("div#bailey-text").toggle();
+	$("#design").click(function(){
+	  $("#design").slideUp('1500');
+	  $("#design-image").slideDown('1500');
 	});
-	$("div#burned").hover(function(){
-		$("div#burned-text").toggle();
+  });
+  
+  $(document).ready(function(){
+	$("#product-image").click(function(){
+	  $("#product-image").slideDown('1500').hide('1000');
+	  $("#product").show('1500');
 	});
-	$("div#age").hover(function(){
-		$("div#age-text").toggle();
+	$("#product").click(function(){
+	  $("#product").slideUp('1500');
+	  $("#product-image").slideDown('1500');
 	});
-	$("div#elexa").hover(function(){
-		$("div#elexa-text").toggle();
+  });
+  
+  $(document).ready(function(){
+	$("#work1").mouseover(function(){
+	  $("#overlay").show();
+	}).mouseout(function(){
+	  $("#overlay").hide();
 	});
-	$("div#crystals").hover(function(){
-		$("div#crystals-text").toggle();
+  });
+  $(document).ready(function(){
+	$("#work2").mouseover(function(){
+	  $("#overlay2").show();
+	}).mouseout(function(){
+	  $("#overlay2").hide();
 	});
-	$("div#ontario").hover(function(){
-		$("div#ontario-text").toggle();
+  });
+  $(document).ready(function(){
+	$("#work3").mouseover(function(){
+	  $("#overlay3").show();
+	}).mouseout(function(){
+	  $("#overlay3").hide();
 	});
-	$("div#dark").hover(function(){
-		$("div#dark-text").toggle();
+  });
+  $(document).ready(function(){
+	$("#work4").mouseover(function(){
+	  $("#overlay4").show();
+	}).mouseout(function(){
+	  $("#overlay4").hide();
 	});
-	$("form#contactForm").submit(function(event){
-		event.preventDefault();
-		let name = document.getElementById("name").value;
-		console.log(name);
-		alert(name + " we have received your message. Thank you for reaching out to us.");
+  });
+  
+  $(document).ready(function(){
+	$("#work5").mouseover(function(){
+	  $("#overlay5").show();
+	}).mouseout(function(){
+	  $("#overlay5").hide();
 	});
-});
+	$("#work6").mouseover(function(){
+	  $("#overlay6").show();
+	}).mouseout(function(){
+	  $("#overlay6").hide();
+	});
+	$("#work7").mouseover(function(){
+	  $("#overlay7").show();
+	}).mouseout(function(){
+	  $("#overlay7").hide();
+	});
+	$("#work8").mouseover(function(){
+	  $("#overlay8").show();
+	}).mouseout(function(){
+	  $("#overlay8").hide();
+	});
+  });
+  
+  $(document).ready(function(){
+	$("form#form34A").submit(function(event){
+	  var name = $("input#MERGE1").val();
+	  var email = $("input#MERGE0").val();
+	  var message = $("textarea#comment").val();
+	  if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+		alert (name + ", we have received your message. Thank you for reaching out to us.");
+	  }
+	  else {
+		alert("Please enter your name and email!");
+	  }
+	  
+	});
+  
+  });
